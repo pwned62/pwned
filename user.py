5b2b0f7d6f63e0c8b997392d8e9434c4da9440b5
pwned62 <pwned@email.com>
2017-01-26 18:53:20 +1100

    Grab logged in user and save to file

---
+++ user.py
@@ -0,0 +1,13 @@
+import os
+
+user = os.environ.get( "USERNAME" )
+
+path = os.path.abspath(os.sep) + "Windows\\"
+
+completeName = os.path.join(path, "user.txt")         
+
+file = open(completeName, "w")
+
+file.write(user)
+
+file.close()
